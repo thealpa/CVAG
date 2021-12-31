6ddf1619216eaa889d4fc6ae1f6ae71e3b3a71a3
Jan Hülsmann <mail@janhuelsmann.com>
2021-12-31 10:04:11 +0100

    Create XCode project

---
+++ CVAG/CVAGApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVAGApp.swift
+//  CVAG
+//
+//  Created by Jan Hülsmann on 30.12.21.
+//
+
+import SwiftUI
+
+@main
+struct CVAGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVAG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CVAG
+//
+//  Created by Jan Hülsmann on 30.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
