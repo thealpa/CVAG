b26ff6ae99b689f83611542d8a55989684bf8d3b
Jan Hülsmann <mail@janhuelsmann.com>
2022-01-02 17:01:57 +0100

    Add stop model

---
+++ CVAG/Models/TransportStop.swift
@@ -0,0 +1,20 @@
+//
+//  TransportStop.swift
+//  CVAG
+//
+//  Created by Jan Hülsmann on 01.01.22.
+//
+
+import Foundation
+import MapKit
+
+struct TransportStop: Identifiable {
+    var id: Int16
+    let name: String
+    let latitude: Double
+    let longitude: Double
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
