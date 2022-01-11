e8899547332809ffaf0c50d9f74cd0be0f8252bf
Jan Hülsmann <mail@janhuelsmann.com>
2022-01-11 18:08:32 +0100

    Animate stop selection

--- CVAG/Views/StopAnnotationView.swift
+++ CVAG/Views/StopAnnotationView.swift
@@ -11,14 +11,23 @@ struct StopAnnotationView: View {
     var stop: Stop
     @Binding var selectedStop: Stop
     @Binding var setDrawerHeight: drawerType
+    @State private var showSelected: Bool = false
     
     var body: some View {
         Image(systemName: "mappin.circle.fill")
             .font(.title)
             .foregroundColor(.blue)
+            .scaleEffect(showSelected ? 1.75 : 1, anchor: .bottom)
+            .animation(.interpolatingSpring(stiffness: 200, damping: 10), value: showSelected)
             .onTapGesture {
                 setDrawerHeight = .medium
                 selectedStop = stop
+                showSelected.toggle()
+            }
+            .onChange(of: selectedStop) { newStop in
+                if stop.id != newStop.id {
+                    showSelected = false
+                }
             }
     }
 }
