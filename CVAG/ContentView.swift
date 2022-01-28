b5a59e2aafc5b514e7f28bd9c5b687b290dd7d42
Jan Hülsmann <mail@janhuelsmann.com>
2022-01-28 21:45:24 +0100

    Set initial drawer position to hidden

--- CVAG/ContentView.swift
+++ CVAG/ContentView.swift
@@ -11,12 +11,12 @@ struct ContentView: View {
     
     // Start with a placeholder stop
     @State var selectedStop: Stop = Stop(id: 0, name: "-", latitude: 0, longitude: 0)
-    @State var setDrawerHeight: drawerType = .variable
+    @State var setDrawerHeight: drawerType = .hidden
     
     var body: some View {
         ZStack {
             MapView(selectedStop: $selectedStop, setDrawerHeight: $setDrawerHeight)
-            DrawerView(selectedStop: $selectedStop, setDrawerHeight: $setDrawerHeight, drawerHeights: drawerDefault)
+            DrawerView(selectedStop: $selectedStop, setDrawerHeight: $setDrawerHeight, drawerHeights: [-100])
         }
     }
 }
